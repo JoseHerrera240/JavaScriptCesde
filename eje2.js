80fc7f869594752513b58bf4c811fdcc83957b68
505 <505@academicos.cesde.edu.co>
2022-03-09 18:32:35 -0500

    JS clase de JavaScript

---
+++ eje2.js
@@ -0,0 +1,21 @@
+/*console.log("Hola mundo desde el lado del servidor");
+let asignatura = "web II";
+let intensidad = 4;
+/*console.log("El nombre de la asignatura es: " + asignatura);*/
+/*console.log(`el nombre de la asignatura es ${asignatura} y su intensidad es ${intensidad*2}`);
+let arreglos = [2, 4, 6, 8, 3, 7, 10, 11, 5];
+for (let p = 0; p< arreglos.length; p++){
+    console.log(`numeros: ${arreglos[p]} y su longitud es ${p}`);
+}
+pos = 0;
+for(valor of arreglos){
+    console.log(`numero del array ${valor}, posiscion ${pos}`);
+    pos++;
+}*/
+
+// primeros numeros de [0,1,1,2,3,5,8,13,21,34,55]
+
+for (let p=0; p<6; p++){
+    console.log(`primero numero: ${p}`);
+}
+/* Generar un algoritmo para listar los N primeros números de esta serie(fibonacci): 0,1,1,2,3,5,8,13,21,34,55...*/
