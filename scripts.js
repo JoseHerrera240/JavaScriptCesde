f4d74539a2fbf04a07ec8bf2760063b2d6286f03
Jose Herrera <hjose0650@gmail.com>
2022-03-13 18:28:50 -0600

    Iniciamos iterador para buscar arrays

---
+++ scripts.js
@@ -0,0 +1,48 @@
+     let productos = [
+            {referencia: 1, descripcion: "PH jumbo", preciounitario: 1000, existencia: 10},
+            {referencia: 2, descripcion: "Ultrex ", preciounitario: 21800, existencia: 56},
+            {referencia: 3, descripcion: "suavizante ", preciounitario: 14000, existencia: 20},
+            {referencia: 4, descripcion: "Limpión", preciounitario: 25000, existencia: 12},
+            {referencia: 5, descripcion: "Toalla de manos", preciounitario: 127000, existencia: 35},
+            {referencia: 6, descripcion: "Neutro ", preciounitario: 14000, existencia: 78},
+            {referencia: 7, descripcion: "Bolsa ", preciounitario: 3400, existencia: 25},
+            {referencia: 8, descripcion: "DEtergente", preciounitario: 12000, existencia: 32},
+            {referencia: 9, descripcion: "Limpia pisos", preciounitario: 9000, existencia: 56},
+            {referencia: 10, descripcion: "astral", preciounitario: 12000, existencia: 10}
+         ]
+        for (let i = 0; i<5; i++){
+                console.log(productos[i])   
+                
+               }
+        // let referencia = parseInt(document.getElementById("referencia").value)
+        //  function search(referencia){
+        //           let  descripcion = document.getElementById("descripcion").value
+        //           let preciounitario = parseInt(document.getElementById("precio").value)
+        //           for (let i = 0; i<productos.length; i++){ 
+        //               if( i == 0 & ){
+        //                 console.log(productos[i])
+        //               }else{
+        //                   console.log("Error")
+        //               }
+        //          }
+        //      }
+        //  let load = document.getElementById('loading');
+        //      load.style.visibility = 'hidden';
+        //  function servidor(){
+        //      setTimeout(function search(){
+        //          let referencia = parseInt(document.getElementById("referencia").value)
+        //          let  descripcion = document.getElementById("descripcion").value
+        //          let preciounitario = parseInt(document.getElementById("precio").value)
+        //          for (let i = 0; i<productos.length; i++){
+        //              if(referencia == productos.referencia){
+        //                 console.log(productos.referencia[referencia])
+        //              }else{
+        //                  console.log("Error")
+        //              }
+        //          }
+
+                
+
+
+        //      },5000)
+        //  }
