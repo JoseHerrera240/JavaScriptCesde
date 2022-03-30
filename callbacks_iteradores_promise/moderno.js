7eedb4c71e8277bd655a368a87569d69e5b8d10e
Jose Herrera <hjose0650@gmail.com>
2022-03-29 18:16:51 -0600

    Realizamos el punto de listar con promesa

--- callbacks_iteradores_promise/moderno.js
+++ callbacks_iteradores_promise/moderno.js
@@ -31,69 +31,69 @@
                 return fncall(valor1,valor2,op);
             }
             //Ejecutar
-            //console.log(fop(fadd));
-            //console.log(fop(fops));
+            console.log(fop(fadd));
+            console.log(fop(fops));
 
-            let load = document.getElementById('loading');
-            load.style.visibility = 'hidden';
-            function servidor(){
-                setTimeout(function(){
-                    let datosper = {di:"123",nombre:"Antonio",salario:2500000};
-                    console.log("Server response");
-                    console.log(`${datosper.di} ${datosper.nombre} ${datosper.salario}`);
-                    document.getElementById('di').value = datosper.di;
-                    document.getElementById('nombre').value = datosper.nombre;
-                    document.getElementById('salario').value = datosper.salario;
-                    document.getElementById('mensaje').innerHTML = "Server response";
+    //         let load = document.getElementById('loading');
+    //         load.style.visibility = 'hidden';
+    //         function servidor(){
+    //             setTimeout(function(){
+    //                 let datosper = {di:"123",nombre:"Antonio",salario:2500000};
+    //                 console.log("Server response");
+    //                 console.log(`${datosper.di} ${datosper.nombre} ${datosper.salario}`);
+    //                 document.getElementById('di').value = datosper.di;
+    //                 document.getElementById('nombre').value = datosper.nombre;
+    //                 document.getElementById('salario').value = datosper.salario;
+    //                 document.getElementById('mensaje').innerHTML = "Server response";
 
-                    load.style.visibility = 'hidden';
-                },5000)
-            }
+    //                 load.style.visibility = 'hidden';
+    //             },5000)
+    //         }
             
-            function fncb(fnc){
-                fnc();
-                console.log("Please wait...");
-                load.style.visibility = 'visible';
-                document.getElementById('mensaje').innerHTML = "Please wait...";
-            }
+    //         function fncb(fnc){
+    //             fnc();
+    //             console.log("Please wait...");
+    //             load.style.visibility = 'visible';
+    //             document.getElementById('mensaje').innerHTML = "Please wait...";
+    //         }
 
-            fncb(servidor);
-    // Promesas
-            let promesa1 = new Promise((resolve,reject)=>{
-                let nota = 5;
-                if (nota >= 4) {
-                    resolve("Tiene derecho al viaje");
-                }else{
-                    reject("NO tiene derecho al viaje");
-                }
-            })
+    //         fncb(servidor);
+    // // Promesas
+    //         let promesa1 = new Promise((resolve,reject)=>{
+    //             let nota = 5;
+    //             if (nota >= 4) {
+    //                 resolve("Tiene derecho al viaje");
+    //             }else{
+    //                 reject("NO tiene derecho al viaje");
+    //             }
+    //         })
 
-            promesa1
-                .then((mensaje)=>{
-                    console.log(mensaje);
-                })
-                .then(()=>{
-                    console.log("...con todos los axcesos...");
-                })
-                .catch(function(error){
-                    console.log(error);
-                })
+    //         promesa1
+    //             .then((mensaje)=>{
+    //                 console.log(mensaje);
+    //             })
+    //             .then(()=>{
+    //                 console.log("...con todos los axcesos...");
+    //             })
+    //             .catch(function(error){
+    //                 console.log(error);
+    //             })
 
-            function fpromesa2(usuario){
-                return new Promise((resolve, reject)=>{
-                    if(usuario=="lruiz"){
-                        let datosper = {nombre:"Lina Ruiz",email:"lruiz@gmail.com"};
-                        resolve(datosper);
-                    }else{
-                        reject("Not found");
-                    }
-                })
-            }
-            fpromesa2("lruiz")
-                .then((data)=>{
-                    console.log(`Nombre: ${data.nombre}, Email: ${data.email}`);
-                })
-                .catch((error)=>{
-                    console.log("Not found");
-                })
+    //         function fpromesa2(usuario){
+    //             return new Promise((resolve, reject)=>{
+    //                 if(usuario=="lruiz"){
+    //                     let datosper = {nombre:"Lina Ruiz",email:"lruiz@gmail.com"};
+    //                     resolve(datosper);
+    //                 }else{
+    //                     reject("Not found");
+    //                 }
+    //             })
+    //         }
+    //         fpromesa2("lruiz")
+    //             .then((data)=>{
+    //                 console.log(`Nombre: ${data.nombre}, Email: ${data.email}`);
+    //             })
+    //             .catch((error)=>{
+    //                 console.log("Not found");
+    //             })
         
--- evaluaciones/MomentoI/script.js
+++ evaluaciones/MomentoI/script.js
@@ -1,15 +1,15 @@
 let inmuebles=[
     {idInmueble: 0, direccion:"", telefono:00,precio:00, estado:"ocupado"},
-    {idInmueble: 1, direccion:"cra 35 67 125", telefono:3152140022,precio:150000000, estado:"disponible"},
+    {idInmueble: 1, direccion:"cra 35 67 125", telefono:3152140022,precio:258840000, estado:"disponible"},
     {idInmueble: 2, direccion:"cra 48 25 06", telefono:3150500427,precio:148000000, estado:"ocupado"},
-    {idInmueble: 3, direccion:"calle 35 67 125", telefono:3151234567,precio:80000000, estado:"disponible"},
-    {idInmueble: 4, direccion:"avenida 35 67 125", telefono:3158567891,precio:100000000, estado:"disponible"},
+    {idInmueble: 3, direccion:"calle 35 67 125", telefono:3151234567,precio:200000000, estado:"disponible"},
+    {idInmueble: 4, direccion:"avenida 35 67 125", telefono:3158567891,precio:250000000, estado:"disponible"},
     {idInmueble: 5, direccion:"cra 62 18 12", telefono:3155677742,precio:110000000, estado:"ocupado"},
-    {idInmueble: 6, direccion:"cra 48 5 15", telefono:315455455,precio:900000000, estado:"disponible"},
-    {idInmueble: 7, direccion:"cra 35 67 125", telefono:315644654,precio:160000000, estado:"disponible"},
-    {idInmueble: 8, direccion:"DGnal 5 45 125", telefono:315245454,precio:128000000, estado:"ocupado"},
-    {idInmueble: 9, direccion:"cra 82 5 05", telefono:3002140022,precio:70000000, estado:"disponible"},
-    {idInmueble: 10, direccion:"cra 80 10 12", telefono:3120505522,precio:50000000, estado:"disponible"}
+    {idInmueble: 6, direccion:"cra 48 5 15", telefono:315455455,precio:450000000, estado:"disponible"},
+    {idInmueble: 7, direccion:"cra 35 67 125", telefono:315644654,precio:168000000, estado:"disponible"},
+    {idInmueble: 8, direccion:"DGnal 5 45 125", telefono:315245454,precio:228000000, estado:"ocupado"},
+    {idInmueble: 9, direccion:"cra 82 5 05", telefono:3002140022,precio:300000000, estado:"disponible"},
+    {idInmueble: 10, direccion:"cra 80 10 12", telefono:3120505522,precio:52000000, estado:"disponible"}
 ]
 let idInmueble =document.getElementById("idInmueble")
 let direccion = document.getElementById("direccion")
@@ -49,7 +49,6 @@ function recorrer(ref){
             document.getElementById('precio').value=inmuebles[i].precio
         }
     }
-    
 }
 
 
@@ -60,6 +59,42 @@ function search(){
 //PROMESA DE TABLA
 function fpinmuebles(arrobj){
     return new Promise((resolve,reject)=>{
-        document.getElementById('mensajepromesa').innerHTML = ""
+        document.getElementById('mensajepromesa').innerHTML = "recuperando los inmuebles"
+        setTimeout(()=>{
+            let mstate = true
+            if(mstate){
+                resolve(arrobj)
+            }else{
+                reject("No hay usuarios que mostrar")
+            }
+        },4000)
     })
 }
+
+//ejecucion de promesa
+fpinmuebles(inmuebles)
+    .then((data)=>{
+        let mihtml = `<table border = 1>
+                        <th>Id del Inmueble</th>
+                        <th>Dirección</th>                    
+                        <th>telefono</th>                    
+                        <th>Precio</th>                    
+                        <th>Estado</th>                    
+        `
+        for(reg of data){
+            if(reg.precio >=  200000000  & reg.precio <=  300000000 & reg.estado == "disponible" ){
+            mihtml += `<tr>
+                        <td>${reg.idInmueble}</td>             
+                        <td>${reg.direccion}</td>             
+                        <td>${reg.telefono}</td>             
+                        <td>${reg.precio}</td>             
+                        <td>${reg.estado}</td>            
+                `
+            }
+        }
+        mihtml += '</table>'
+        document.getElementById('mensajepromesa').innerHTML=mihtml
+    })
+    .catch((error)=>{
+        document.getElementById('mensajepromesa').innerHTML=error
+    })
